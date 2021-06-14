a99b7813f1edf625f83e2d38d2112eeff33ea99f
BThanatonz <43448460+banksss33@users.noreply.github.com>
2021-06-14 21:46:11 +0700

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -1,8 +1,19 @@
 #selection sorting from rookie using for and while loop
 #หลักการทำงานคร่าวๆคือ ระบบจะทำการหาเลขที่น้อยที่สุดและเก็บไว้ในค่า minimum จากนั้นหาตำแหน่ง index ของเลข minimum นั้น
 #และทำการลบ elements ใน index ที่ให้มาหลังจากนั้นก็เอาค่า minimum ไปใส่ไว้ใน List อีกอัน
+def randomnumlist(length):
+    import random 
+    numberlist = []
+    count = 0
+    while count != length:
+        randnum = random.randint(1,9) #randomnumber between 1-9
+        numberlist.append(randnum) #put the randomnumber to the List
+        count+=1
+    return numberlist
 
-Number = [2,9,7,8,4,5,3,1,6] #Number that need to be Sorted
+length = int(input("List length: ")) #input the length of the List
+Number = randomnumlist(length) #Number that need to be Sorted
+print('Number that need to be Sorted: ',Number)
 SortedNumber = [] #Array For SortedNumber
 length = len(Number) #find the length of Array
 count = 0
@@ -17,4 +28,4 @@ while count != length:
 	Number.pop(indexlocation)#then remove elements of that index
 	SortedNumber.append(minimum)#add the minimum elements to the list
 	count+=1
-print(SortedNumber)
+print("Sorted: ",SortedNumber)
